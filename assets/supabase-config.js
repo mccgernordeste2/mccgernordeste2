bbd6b9c5bc8409db9420f9cb753e422da971507e
mccgernordeste2 <gernordeste2@gmail.com>
2026-09-25 17:11:08 -0300

    Adiciona configuracao publica do Supabase

---
+++ assets/supabase-config.js
@@ -0,0 +1,3 @@
+window.MCC_SUPABASE_URL='https://krupjfytxnndfjfwubmw.supabase.co';
+window.MCC_SUPABASE_KEY='sb_publishable_1CRWbMPTKqxfTGnlKnhdjQ_DAdWPRtL';
+window.mccSupabase = window.supabase.createClient(window.MCC_SUPABASE_URL, window.MCC_SUPABASE_KEY);
